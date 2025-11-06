584fcce92b00e30a28ca88b41b322534fd4697ae
Madhav Madhusoodanan <f20200049@pilani.bits-pilani.ac.in>
2025-11-06 11:08:05 +0530

    chore: remove the clippy annotations from the kernel-side code

--- samples/introduction/async_api/kernels/src/lib.rs
+++ samples/introduction/async_api/kernels/src/lib.rs
@@ -1,7 +1,6 @@
 use cuda_std::prelude::*;
 
 #[kernel]
-#[allow(improper_ctypes_definitions, clippy::missing_safety_doc)]
 pub unsafe fn increment(g_data: *mut u32, inc_value: u32) {
     // This can also be obtained directly as
     //
